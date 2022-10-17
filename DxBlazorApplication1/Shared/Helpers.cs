a80cd1588efb43dc35a25e6088f9fc6cb9e31375
Harris Kisseoglou <hkis@elpho.gr>
2022-10-17 13:25:35 +0300

    temp checkin

---
+++ DxBlazorApplication1/Shared/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+
+namespace DxBlazorApplication1.Shared
+{
+    public class Helpers
+    {
+        public static async Task<string> GetCookieAsync(IJSRuntime JsRuntime, string cookieName)
+        {
+            return await JsRuntime.InvokeAsync<string>("ReadCookie.ReadCookie", cookieName);
+        }
+
+        public static async Task SetCookieAsync(IJSRuntime JsRuntime, string cookieName, string cookieValue, int numDays)
+        {
+            await JsRuntime.InvokeVoidAsync("WriteCookie.WriteCookie", cookieName, cookieValue, numDays);
+        }
+
+        public static void SetCookie(IJSRuntime JsRuntime, string cookieName, string cookieValue, int numDays2Expire)
+        {
+            JsRuntime.InvokeVoidAsync("WriteCookie.WriteCookie", cookieName, cookieValue, numDays2Expire);
+        }
+    }
+}
